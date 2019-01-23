95313506e5f8d09873c86d4bbc0febe0410e7adf
Nirmal Kanagasabai <nirmal.kanagasabai@gmail.com>
2019-01-22 23:01:16 -0500

    Transpiled code
    Introduction to TypeScript
    - Arrow Function (in JS)
    - How is Type-Declaration done?
    - Diff. b/w Java & TypeScript in terms of Type Declaration? Pre & Postfix Declaration?

--- hello-world.js
+++ hello-world.js
@@ -1,27 +1,19 @@
 var normalFunction = function () {
     return Response;
-}
-
+};
 // Arrow Functions
-var arrowFunction = () => 'response';
-
+var arrowFunction = function () { return 'response'; };
 // TypeScript allows type-declaration
 // TypeScript is a strongly-typed language
-
 // Ensures that a type-declaration is done i.e., given a variable, we can declare what type the variable is. 
-var a: number;
-
+var a;
 a = 50;
 // a = true;
 // hello-world.ts:14:1 - error TS2322: Type 'true' is not assignable to type 'number'.
-
 // All primitive JS types are valid TS types too. 
-
-var a: number;
-var b: boolean;
-var c: string;
-
+var a;
+var b;
+var c;
 // PREFIX (Java) vs. POSTFIX DECLARATION (TypeScript)
 // PREFIX: In Java, the declaration of the type of the variable is done before it. For example, it is, 'int a', 'float b', 'String c', etc.
 // POSTFIX: In TypeScript, the declaration of the type of the variable is done after it. 
-
